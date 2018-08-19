e0124a04a144bc3fb7fef71180f9439b672f6c84
r614 <35357371+r614@users.noreply.github.com>
2018-08-19 01:50:58 -0700

    Considers duplicates and filters.

--- crawler.py
+++ crawler.py
@@ -21,7 +21,7 @@ path = r"D:/School/Python/Rating/chromedriver.exe"
 max_teams = 50
 
 # Open up a chrome browser and get IM Standings page
-driver = webdriver.Chrome(path)
+driver = webdriver.Chrome()
 driver.get(standings_url)
 
 element = WebDriverWait(driver, 30).until(
@@ -57,22 +57,31 @@ for i in range(0, len(table_rows)):
                 team_stats[5] = cells[5].find('a').text
                 team_stats[6] = cells[6].find('a').text
                 team_stats_list.append(team_stats)
-                with open(r'stats.csv', 'a') as f:
-                    writer = csv.writer(f)
+                with open('stats_2', 'w', newline='') as outfile:
+                    writer = csv.writer(outfile)
                     writer.writerow(team_stats)
     time.sleep(4)
 
 # big_data = np.genfromtxt('stats.csv', delimiter=',')
 
 # Creates an array for every row in team__stats_list. Checks for presence in stats_list_filtered. If present, doesn't append.
+r = 0
 length = len(team_stats_list)
-for r in range(0, length):
+while r < length:
     search = team_stats_list[r]
-    for i in range(0, len(stats_list_filtered)):
-        if(search[1:] == stats_list_filtered[i][1:]):
-            pass
-        else:
-            stats_list_filtered.append(search)
-            with open(r'test_filter.csv', 'a') as f:
-                writer = csv.writer(f)
-                writer.writerow(stats_list_filtered)
+    if (search[0] == 'Animus Gaming'and search[-1] == 'Jul 9 18'):
+        stats_list_filtered.append(search[:])
+        r = length
+        print(stats_list_filtered)
+    else:
+        for i in range(0, len(stats_list_filtered)):
+            if(search[1:] == stats_list_filtered[i][1:]):
+                pass
+            else:
+                stats_list_filtered.append(search[:])
+                print(stats_list_filtered)
+                r = r + 1
+                break
+with open('stats_filtered', 'w', newline=',') as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(team_stats)
--- test.py
+++ test.py
@@ -2,20 +2,31 @@
 import csv
 
 
-with open('stats', 'r') as f:
+with open('stats', 'r', newline='') as f:
     reader = csv.reader(f)
     team_stats_list = list(reader)
+    print(team_stats_list)
+
 length = len(team_stats_list)
 
-stats_list_filtered = ['', '',  '',  '', '', '', '']
-for r in range(0, length):
+stats_list_filtered = []
+r = 0
+while r < length:
     search = team_stats_list[r]
-    for i in range(0, len(stats_list_filtered)):
-        if(search[1:] == stats_list_filtered[i][1:]):
-            pass
-        else:
-            stats_list_filtered.append(search)
+    if (search[0] == 'Animus Gaming'and search[-1] == 'Jul 9 18'):
+        stats_list_filtered.append(search[:])
+        r = length
+        print(stats_list_filtered)
+    else:
+        for i in range(0, len(stats_list_filtered)):
+            if(search[1:] == stats_list_filtered[i][1:]):
+                pass
+            else:
+                stats_list_filtered.append(search[:])
+                print(stats_list_filtered)
+                r = r + 1
+                break
 
-with open(r'test_filter.csv', 'a') as f:
-    writer = csv.writer(f)
+with open('stats_filtered.csv', 'w', newline='') as outfile:
+    writer = csv.writer(outfile)
     writer.writerows(stats_list_filtered)
